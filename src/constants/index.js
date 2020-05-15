2cd816403e89e5fc120a5230f0885e5c0b3e4525
shellteo <shellteo@163.com>
2020-05-15 17:07:55 +0800

    update contract address

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import { injected, walletconnect, walletlink, fortmatic, portis, torus } from '.
 export const FACTORY_ADDRESSES = {
   1: '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
   3: '0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351',
-  4: '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36',
+  4: '0xc4d477bcf1578ebf4dee2318e19ab7649a83f5bb',
   42: '0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30'
 }
 
